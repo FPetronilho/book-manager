9a1569adf4c34fa52c3676f89fa5d0c291ff23db
FPetronilho <jfgp_10@live.com.pt>
2025-03-25 00:06:44 +0100

    Created essential modules and updated pom files accordingly.

--- book-manager-application/src/main/java/com/tracktainment/bookmanager/BookManagerApplication.java
+++ book-manager-application/src/main/java/com/tracktainment/bookmanager/BookManagerApplication.java
@@ -1,4 +1,4 @@
-package com.tracktainment.book_manager;
+package com.tracktainment.bookmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class BookManagerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BookManagerApplication.class, args);
 	}
-
 }
